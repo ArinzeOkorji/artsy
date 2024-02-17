a10efc3a5de0bbf32f5f5183504a87a5e02131c8
Arinze Okorji <arinzeokorji@yahoo.com>
2024-02-17 17:18:50 +0100

    feat: Multiple user drawing board using socket.io

--- index.js
+++ index.js
@@ -15,66 +15,125 @@ let isDrawing = false;
 let shape = '';
 const canvasColor = '#fff';
 let color = '#000';
-let bounds;
-
-colorPicker.addEventListener('change', (e) => {
-    color = e.target.value;
-})
+let bounds = canvas.getBoundingClientRect();
+const socket = io('http://localhost:3000/')
 
 
-function selectShape(name) {
-    shape = name
-}
+window.addEventListener('load', () => {
+    const userId = Math.random() * 1000;
+    socket.on('connected', () => {
+    })
+    socket.on('drawLine', (newLine) => {
+        prepareToRecieveDrawing();
+        drawLines(newLine, newLine.color, false)
+    })
+    socket.on('drawTriangle', (newTriangle) => {
+        prepareToRecieveDrawing();
+        drawTriangles(newTriangle, newTriangle.color, false)
+    })
+    socket.on('drawRectangle', (newRectangle) => {
+        prepareToRecieveDrawing();
+        drawRectangle(newRectangle, newRectangle.color, false)
+    })
+    socket.on('drawCircle', (newCircle) => {
+        prepareToRecieveDrawing();
+        drawCircle(newCircle, newCircle.color, false)
+    })
+    socket.on('drawFreely', (newFreeDraw) => {
+        prepareToRecieveDrawing('freeDraw');
+        drawFreely(newFreeDraw, newFreeDraw.color, false)
+    })
+    socket.on('erase', (eraser) => {
+        prepareToRecieveDrawing('erase');
+        erase(eraser, false)
+    })
+    socket.on('stop-draw', (state) => {
+        sessionStorage.removeItem('continue-draw');
+    })
 
-    canvas.width = canvasContainer.offsetWidth;
-    canvas.height = canvasContainer.offsetHeight;
-if (canvas.getContext) {
-    canvasContext = canvas.getContext('2d');
-
-    function drawLines(move) {
-        resetCanvas()
-        const x = move.x - bounds.left - scrollX;
-        const y = move.y - bounds.top - scrollY;
-        if (lineStart) {
-            lastX = x;
-            lastY = y;
-            lineStart = false;
+    function prepareToRecieveDrawing(shape = '') {
+        if (!sessionStorage.getItem('continue-draw')) {
+            shape !== 'freeDraw' && shape !== 'erase' ? snapshot = canvasContext.getImageData(0, 0, canvas.width, canvas.height) : null;
+            sessionStorage.setItem('continue-draw', true);
         }
-        canvasContext.beginPath();
-        canvasContext.moveTo(lastX, lastY);
-        canvasContext.lineTo(lastX, lastY)
-        canvasContext.lineTo(x, y);
 
-        canvasContext.strokeStyle = color;
-
-        canvasContext.stroke();
-
-    }
-
-    function drawTriangles(move) {
-        resetCanvas()
-        const x = move.x - bounds.left - scrollX;
-        const y = move.y - bounds.top - scrollY;
-        if (lineStart) {
-            lastX = x;
-            lastY = y;
-            lineStart = false;
-        }
-        const nextX = x > lastX ? lastX - (x - lastX) : lastX + (lastX - x);
-        const nextY = lastY;
-        canvasContext.beginPath();
-            canvasContext.moveTo(lastX, lastY);
-        canvasContext.lineTo(x, y);
-        canvasContext.lineTo(nextX, y);
-
-        canvasContext.fillStyle = color
-        canvasContext.fill();
-
+        shape !== 'freeDraw' && shape !== 'erase' ? canvasContext.putImageData(snapshot, 0, 0) : null;
     }
 
 
-    function drawFreely(move) {
-        resetCanvas();
+    if (canvas.getContext) {
+        canvasContext = canvas.getContext('2d');
+
+        function drawLines(move, color, emit = true) {
+            const x = move.x - bounds.left - scrollX;
+            const y = move.y - bounds.top - scrollY;
+            if (lineStart) {
+                lastX = x;
+                lastY = y;
+                lineStart = false;
+            }
+
+            canvasContext.beginPath();
+            if (!emit) {
+                canvasContext.moveTo(move.lastX, move.lastY);
+                canvasContext.lineTo(move.lastX, move.lastY)
+                canvasContext.lineTo(move.x, move.y);
+                canvasContext.strokeStyle = move.color;
+            } else {
+                canvasContext.moveTo(lastX, lastY);
+                canvasContext.lineTo(lastX, lastY)
+                canvasContext.lineTo(x, y);
+                canvasContext.strokeStyle = color;
+                const line = {
+                    lastX,
+                    lastY,
+                    x,
+                    y,
+                    color
+                }
+                socket.emit('drawLine', line);
+            }
+            canvasContext.stroke();
+        }
+
+        function drawTriangles(move, color, emit = true) {
+            const x = move.x - bounds.left - scrollX;
+            const y = move.y - bounds.top - scrollY;
+            if (lineStart) {
+                lastX = x;
+                lastY = y;
+                lineStart = false;
+            }
+            canvasContext.beginPath();
+
+            if (!emit) {
+                canvasContext.moveTo(move.lastX, move.lastY);
+                canvasContext.lineTo(move.x, move.y);
+                canvasContext.lineTo(move.nextX, move.y);
+                canvasContext.fillStyle = move.color
+            } else {
+                const nextX = x > lastX ? lastX - (x - lastX) : lastX + (lastX - x);
+                canvasContext.moveTo(lastX, lastY);
+                canvasContext.lineTo(x, y);
+                canvasContext.lineTo(nextX, y);
+                canvasContext.fillStyle = color
+
+                const triangle = {
+                    lastX,
+                    lastY,
+                    x,
+                    y,
+                    color,
+                    nextX
+                }
+                socket.emit('drawTriangle', triangle);
+            }
+
+            canvasContext.fill();
+        }
+
+
+        function drawFreely(move, color, emit = true) {
             const x = move.pageX - bounds.left - scrollX;
             const y = move.pageY - bounds.top - scrollY;
             if (lineStart) {
@@ -83,54 +142,93 @@ if (canvas.getContext) {
                 lineStart = false;
             }
             canvasContext.beginPath();
-            canvasContext.lineTo(lastX, lastY)
-            canvasContext.lineTo(x, y);
-            canvasContext.strokeStyle = color
+            if (!emit) {
+                canvasContext.lineTo(move.lastX, move.lastY)
+                canvasContext.lineTo(move.x, move.y);
+                canvasContext.strokeStyle = move.color;
+            } else {
+                canvasContext.lineTo(lastX, lastY);
+                canvasContext.lineTo(x, y);
+                canvasContext.strokeStyle = color;
+
+                const freeStyle = {
+                    lastX,
+                    lastY,
+                    x,
+                    y,
+                    color
+                }
+
+                socket.emit('drawFreely', freeStyle);
+            }
             canvasContext.stroke();
-            canvasContext.closePath()
+            canvasContext.closePath();
             lastX = x;
-            lastY = y
+            lastY = y;
 
-    }
+        }
 
-    function drawRectangle(move) {
-        resetCanvas();
-        let x = move.x - bounds.left - scrollX;
-        let y = move.y - bounds.top - scrollY;
+        function drawRectangle(move, color, emit = true) {
+            let x = move.x - bounds.left - scrollX;
+            let y = move.y - bounds.top - scrollY;
 
-        const width = x - lastX;
-        const height = y - lastY;
-
-        // canvas.clearRect()
-        console.log(lastX, lastY);
-        canvasContext.strokeStyle = color
-        canvasContext.strokeRect(lastX, lastY, width, height)
-        
+            const width = x - lastX;
+            const height = y - lastY;
 
 
-    }
-    function drawCircle(move) {
-        resetCanvas();
-        let x = move.x - bounds.left - scrollX;
-        let y = move.y - bounds.top - scrollY;
+            if (!emit) {
+                canvasContext.strokeStyle = move.color;
+                canvasContext.strokeRect(move.lastX, move.lastY, move.width, move.height);
+            } else {
+                canvasContext.strokeStyle = color;
+                canvasContext.strokeRect(lastX, lastY, width, height);
 
-        const newX = x - lastX;
-        const newY = y - lastY;
+                const rectangle = {
+                    lastX,
+                    lastY,
+                    width,
+                    height,
+                    color
+                }
 
-        const radius = Math.pow(newX, 2) + Math.pow(newY, 2)
+                socket.emit('drawRectangle', rectangle);
+            }
 
 
-        canvasContext.beginPath();
-        canvasContext.arc(lastX, lastY, radius, 0, Math.PI * 2, false);
-        canvasContext.strokeStyle = color;
-        canvasContext.stroke();
-        
+
+        }
+        function drawCircle(move, color, emit = true) {
+            let x = move.x - bounds.left - scrollX;
+            let y = move.y - bounds.top - scrollY;
+
+            canvasContext.beginPath();
+            if (!emit) {
+                canvasContext.arc(move.lastX, move.lastY, move.radius, 0, Math.PI * 2, false);
+                canvasContext.strokeStyle = move.color;
+            } else {
+                const newX = x - lastX;
+                const newY = y - lastY;
+
+                const radius = Math.pow(newX, 2) + Math.pow(newY, 2)
 
 
-    }
+                canvasContext.arc(lastX, lastY, radius, 0, Math.PI * 2, false);
+                canvasContext.strokeStyle = color;
 
-    function erase(move) {
-        resetCanvas();
+                const circle = {
+                    lastX,
+                    lastY,
+                    radius,
+                    color
+                }
+
+                socket.emit('drawCircle', circle);
+            }
+            canvasContext.stroke();
+
+        }
+
+        function erase(move, emit = true) {
             const x = move.pageX - bounds.left - scrollX;
             const y = move.pageY - bounds.top - scrollY;
             if (lineStart) {
@@ -141,59 +239,98 @@ if (canvas.getContext) {
             canvasContext.lineWidth = 10;
             canvasContext.strokeStyle = canvasColor
             canvasContext.beginPath();
-            canvasContext.lineTo(lastX, lastY)
-            canvasContext.lineTo(x, y)
+            if (!emit) {
+                canvasContext.lineTo(move.lastX, move.lastY);
+                canvasContext.lineTo(move.x, move.y);
+            } else {
+                canvasContext.lineTo(lastX, lastY);
+                canvasContext.lineTo(x, y);
+
+                const eraser = {
+                    lastX,
+                    lastY,
+                    x,
+                    y
+                }
+
+                socket.emit('erase', eraser);
+            }
+                lastX = x;
+                lastY = y;
             canvasContext.stroke();
-            canvasContext.closePath()
-            lastX = x;
-            lastY = y
+            canvasContext.closePath();
 
-    }
-
-    function prepareToDraw(e) {
-        isDrawing = true;
-        bounds = canvas.getBoundingClientRect();
-        lastX = e.x - bounds.left - scrollX;
-        lastY = e.y - bounds.top - scrollY;
-        console.log(lastX, lastY)
-        shape !== 'freeDraw' && shape !== 'erase' ? snapshot = canvasContext.getImageData(0, 0, canvas.width, canvas.height) : null;
-    }
-
-    function draw(e) {
-        if (!isDrawing) {
-            return
         }
 
-
-        shape !== 'freeDraw' && shape !== 'erase' ? canvasContext.putImageData(snapshot, 0, 0) : null;
-        isDrawing = true;
-
-        switch (shape) {
-            case 'line':
-                drawLines(e);
-                break;
-            case 'freeDraw':
-                drawFreely(e);
-                break;
-            case 'rectangle':
-                drawRectangle(e);
-                break;
-            case 'circle':
-                drawCircle(e);
-                break;
-            case 'triangle':
-                drawTriangles(e);
-                break;
-            case 'erase':
-                erase(e);
-                break;
+        function prepareToDraw(e) {
+            lineStart = true
+            isDrawing = true;
+            lastX = e.x - bounds.left - scrollX;
+            lastY = e.y - bounds.top - scrollY;
+            shape !== 'freeDraw' && shape !== 'erase' ? snapshot = canvasContext.getImageData(0, 0, canvas.width, canvas.height) : null;
         }
-    }
 
-    canvas.addEventListener('mousedown', prepareToDraw);
-    canvas.addEventListener('mousemove', draw);
-    canvas.addEventListener('mouseup', () => {isDrawing = false})
+        function draw(e) {
+            if (!isDrawing) {
+                return
+            }
+
+
+            shape !== 'freeDraw' && shape !== 'erase' ? canvasContext.putImageData(snapshot, 0, 0) : null;
+            isDrawing = true;
+
+            switch (shape) {
+                case 'line':
+                    resetCanvas();
+                    drawLines(e, color, true);
+                    break;
+                case 'freeDraw':
+                    resetCanvas();
+                    drawFreely(e, color, true);
+                    break;
+                case 'rectangle':
+                    resetCanvas()
+                    drawRectangle(e, color, true);
+                    break;
+                case 'circle':
+                    resetCanvas();
+                    drawCircle(e, color, true);
+                    break;
+                case 'triangle':
+                    resetCanvas();
+                    drawTriangles(e, color, true);
+                    break;
+                case 'erase':
+                    resetCanvas();
+                    erase(e, true);
+                    break;
+            }
+        }
+
+        canvas.addEventListener('mousedown', prepareToDraw);
+        canvas.addEventListener('mousemove', draw);
+        canvas.addEventListener('mouseup', () => {
+            isDrawing = false;
+            socket.emit('stop-draw', true)
+        })
+    }
+})
+
+function connectToSocket() {
 }
+
+colorPicker.addEventListener('change', (e) => {
+    color = e.target.value;
+})
+
+
+function selectShape(name) {
+    shape = name
+}
+
+canvas.width = canvasContainer.offsetWidth;
+canvas.height = canvasContainer.offsetHeight;
+
 
 
 
@@ -203,6 +340,6 @@ function resetCanvas() {
     canvas.removeEventListener('mousedown', mouseDownMechanism)
     canvas.removeEventListener('mouseup', mouseUpMechanism);
     canvasContext.lineWidth = 1;
-            canvasContext.strokeStyle = "#000"
+    canvasContext.strokeStyle = "#000"
     // canvasContext = undefined;
 }
---
+++ server/index.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors, { CorsOptions } from 'cors';
+
+const app = express();
+const httpServer = createServer(app);
+
+const corsOptions: CorsOptions = {
+    origin: [
+        "http://127.0.0.1:5500"
+    ]
+}
+
+app.use(cors(corsOptions));
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://127.0.0.1:5500"
+    }
+});
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Socket')
+})
+
+io.on('connection', (socket) => {
+    socket.on('drawLine', (line) => {
+        socket.broadcast.emit('drawLine', line);
+    })
+    socket.on('drawTriangle', (triangle) => {
+        socket.broadcast.emit('drawTriangle', triangle);
+    })
+    socket.on('drawRectangle', (rectangle) => {
+        socket.broadcast.emit('drawRectangle', rectangle);
+    })
+    socket.on('drawCircle', (circle) => {
+        socket.broadcast.emit('drawCircle', circle);
+    })
+    socket.on('drawFreely', (freeDraw) => {
+        socket.broadcast.emit('drawFreely', freeDraw);
+    })
+    socket.on('erase', (erase) => {
+        socket.broadcast.emit('erase', erase);
+    })
+    socket.on('stop-draw', (state) => {
+        socket.broadcast.emit('stop-draw', state);
+    })
+})
+
+app.use(express.json());
+
+httpServer.listen(port, () => {
+    console.log('app is running on port ' + port)
+})
